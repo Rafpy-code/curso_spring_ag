f2dfecd5a1ae42901d25a8ed845f88945d136db8
rafpy <castor.ardilla.me@gmail.com>
2024-02-05 12:19:22 +0100

    h2 y mysql funcionando crud

---
+++ spring-boot-cliente-jpa-mysql/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Rapuncel', 'Cel', 'rapuncel@cel.com', '2024-01-25');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Perico', 'Te', 'pericote@gmail.com', '2024-01-28');
